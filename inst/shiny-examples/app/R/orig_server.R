b8334cc3a760a09b4f7b3c7dfadf8e6cc04e17c8
bernice99 <bcu120@aucklanduni.ac.nz>
2020-02-04 11:37:48 +1300

    corrected get_agg_ins

--- inst/shiny-examples/app/R/orig_server.R
+++ inst/shiny-examples/app/R/orig_server.R
@@ -274,7 +274,8 @@ insighted_agg <- reactive({
     "Aggregated Sentiment" = get_aggregate_insight(grouped(),
                                                    c("Bound Aggregates", input$what_vis),
                                                    input$agg_var,
-                                                   input$sent_lex), to_scale = input$scale_senti)
+                                                   input$sent_lex, 
+                                                   to_scale = input$scale_senti))
   })
 
 
